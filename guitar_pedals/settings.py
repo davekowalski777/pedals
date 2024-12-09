ba6008d002199bcaaa0261947bf8eab2153eb5c1
davekowalski777 <davkovsky@gmail.com>
2024-12-09 21:02:21 +0100

    Update settings.py

--- guitar_pedals/settings.py
+++ guitar_pedals/settings.py
@@ -107,11 +107,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Media files
 MEDIA_URL = '/media/'
