5eb31568032e25e9c8e532e13c5ae1369b1b5448
davekowalski777 <davkovsky@gmail.com>
2024-12-09 20:32:20 +0100

    Update django.py

--- netlify/functions/django.py
+++ netlify/functions/django.py
@@ -1,5 +1,5 @@
 from guitar_pedals.wsgi import application
-from netlify_wsgi import handler
+import awsgi
 
-def handle(event, context):
-    return handler(event, context, application)
+def handler(event, context):
+    return awsgi.response(application, event, context)
