4137c47a1c469266fd7cdc110fc73f4868d2db88
davekowalski777 <dave@iMac-Dave.local>
2024-12-14 22:39:01 +0100

    Fixed settings module path in manage.py to use guitar_pedals.settings

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pedal_database.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'guitar_pedals.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
